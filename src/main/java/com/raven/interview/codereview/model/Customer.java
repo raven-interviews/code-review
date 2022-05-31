99b9a71970473f02da978743b55ffcc090d855fd
Raven Interviews <106619907+raven-interviews@users.noreply.github.com>
2022-05-31 16:26:45 -0400

    Add support classes.

---
+++ src/main/java/com/raven/interview/codereview/model/Customer.java
@@ -0,0 +1,68 @@
+package com.raven.interview.codereview.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long customerId;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String email;
+
+    public Customer() {}
+
+    public Customer(String firstName, String lastName, Date dateOfBirth, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/raven/interview/codereview/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.raven.interview.codereview.repository;
+
+import com.raven.interview.codereview.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
